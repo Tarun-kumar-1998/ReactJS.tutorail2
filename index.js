d7e875e8209504255b95c54b6d2e6d27cb3e7296
Tarun-kumar-1998 <tarunvankayalapati@gmail.com>
2021-07-05 06:59:15 +0000

    tutorail

---
+++ index.js
@@ -0,0 +1,23 @@
+const element = (
+  <div className="bg">
+    <h1 className="heading">Congratulations</h1>
+    <div classname="bg1">
+      <img
+        className="img"
+        src="https://res.cloudinary.com/dvi3geadn/image/upload/v1618386274/49621842_434826397056359_7147215673154624856_n.jpg_ggtze6.jpg"
+      />
+      <h1 className="heading">taRUN kuMAR Vankayalapati</h1>
+      <p>
+        Lakireddy balireddy collage of engineering Electrical and electronics of
+        engineering , Mylavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
